7ec4d8f2d1d53d7c97141fad49a743b1bfa205d1
victoriatrinita <victoriapardede@gmail.com>
2022-05-29 06:45:13 +0700

    feat: find all users

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,30 @@
+const db = require('../models');
+const User = db.user;
+
 exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+	res.status(200).send('Public Content.');
 };
 
 exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+	res.status(200).send('User Content.');
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+	res.status(200).send('Admin Content.');
 };
 
 exports.instructorBoard = (req, res) => {
-  res.status(200).send("Instructor Content.");
+	res.status(200).send('Instructor Content.');
+};
+
+exports.findAll = (req, res) => {
+	User.find({})
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || 'error retrieving users',
+			});
+		});
 };
